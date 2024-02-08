5aa1d2d3ab2ffa400cf45d549e5428a17a53af11
jacottemonroe <jacottemonroe@gmail.com>
2024-02-08 05:44:14 -0500

    Reorganized code with comments to lead flow of script (because script doesn't flow). Dplyr 'group_by' function doesn't work properly when amt package loaded so temporary workaround is to clear program halfway through running code for now. Also added code to get largest extent, code to stack MODIS images, and code to extract covariates (not finished).

--- main_model_building.R
+++ main_model_building.R
@@ -2,48 +2,23 @@
 ## Jacotte Monroe 
 ## Model Building 
 
+###############################################################################
+###### START BY RUNNING THIS PART OF THE SCRIPT UNTIL BREAK ###################
+###############################################################################
 
 ## libraries 
 if(!('terra') %in% installed.packages()){install.packages('terra')}
 library(terra)
 if(!('tidyterra') %in% installed.packages()){install.packages('tidyterra')} # for mapping spatraster
-library(tidyterra)
+#library(tidyterra)
 if(!('ggplot2') %in% installed.packages()){install.packages('ggplot2')}
-library(ggplot2)
+#library(ggplot2)
 if(!('lubridate') %in% installed.packages()){install.packages('lubridate')}
 library(lubridate)
-if(!('amt') %in% installed.packages()){install.packages('amt')}
-library(amt)
-
 if(!('dplyr') %in% installed.packages()){install.packages('dplyr')} #for grouping in table (max/min)
 library(dplyr)
-
-
- 
-########################### load MODIS data ################################
-
-# ## load MODIS NDVI image --> random one for now 
-# modis <- rast('data/MODIS_250m_feb17_2014_sampleLA2.tif')
-# 
-# # remove pixels with outside of study area (assign NA)
-# modis[modis == 0] <- NA
-# 
-# # calculate NDVI
-# modis$ndvi <- (modis$sur_refl_b02 - modis$sur_refl_b01)/(modis$sur_refl_b02 + modis$sur_refl_b01)
-# 
-# # NDVI values above 1 were originally masked pixels (assign NA)
-# modis[modis$ndvi > 1] <- NA
-# modis$ndvi[modis$ndvi < 0] <- 0
-# 
-# # map NDVI 
-# # source: https://dieghernan.github.io/tidyterra/reference/geom_spatraster.html
-# # make sure to have loaded tidyterra package and ggplot2 
-# # source: https://stackoverflow.com/questions/75074597/plotting-rasterlayer-objects-using-ggplot
-# modis_ndvi_map <- ggplot() + 
-#   geom_spatraster(data = modis, aes(fill = ndvi), show.legend = T) + 
-#   scale_fill_terrain_c(name = 'NDVI')
-# 
-# modis_ndvi_map
+if(!('amt') %in% installed.packages()){install.packages('amt')}
+library(amt)
 
 
 
@@ -84,28 +59,46 @@ df$date_time <- as.POSIXct(df$date_time, tz = 'Africa/Maputo')
 # 'burst_' is a fixed/mandatory column name if want to generate steps for multiple paths
 tr <- make_track(df, location.long, location.lat, date_time, week = week, burst_ = path)
 
-# summary to check step interval --> it is correctly 4h nice
-#summarize_sampling_rate(tr)
 
 
-
-########################## generate steps ##############################
+########################## generate steps ############################## 
 
 # turn elephant track dataset into steps
 # by burst so steps not created for fixes in different paths
 elephant_steps_by_path <- steps_by_burst(tr)
 
 # generate corresponding random steps
+set.seed(1234)
 elephant_all_steps_by_path <- random_steps(elephant_steps_by_path, n_control = 20,
                                            sl_distr = fit_distr(elephant_steps_by_path$sl_, "gamma"),
                                            ta_distr = fit_distr(elephant_steps_by_path$ta_, "vonmises"))
 
-# save table 
+# save table (TEMPORARY FIX TO PROBLEM)
 write.csv(elephant_all_steps_by_path, 'data/temp_eleph_path.csv')
 
 
 
-######################### step extents table ##########################
+
+###############################################################################
+###### CLOSE AND RESTART PROGRAM WHEN GET HERE TO CLEAR HISTORY ###############
+###### START RUNNING THE CODE AFTER THIS BREAK DONT LOAD AMT PACKAGE ##########
+###############################################################################
+
+
+## libraries 
+if(!('terra') %in% installed.packages()){install.packages('terra')}
+library(terra)
+if(!('tidyterra') %in% installed.packages()){install.packages('tidyterra')} # for mapping spatraster
+#library(tidyterra)
+if(!('ggplot2') %in% installed.packages()){install.packages('ggplot2')}
+#library(ggplot2)
+if(!('lubridate') %in% installed.packages()){install.packages('lubridate')}
+library(lubridate)
+if(!('dplyr') %in% installed.packages()){install.packages('dplyr')} #for grouping in table (max/min)
+library(dplyr)
+
+
+######################### step extents table ########################## 
 
 # could make this code more efficient and less repetitive but idk how for now
 eleph_mov <- read.csv('data/temp_eleph_path.csv') #elephant_all_steps_by_path
@@ -134,75 +127,88 @@ step_ex <- step_ex %>% group_by(start_date) %>% mutate(xmin = min(c(x1_, x2_))) 
 # get table of all extents per date
 step_ex <- unique(step_ex[,c('start_date', 'end_date', 'start_date_prev_week', 'end_date_prev_week', 'xmin', 'ymin', 'xmax', 'ymax')])
 
+# add largest extent as new row in step extent LUT 
+# source: https://www.rdocumentation.org/packages/terra/versions/1.7-71/topics/ext
+step_ex <- rbind(step_ex, data.frame('start_date' = NA, 'end_date' = NA, 'start_date_prev_week' = NA,
+                                     'end_date_prev_week' = NA, 'xmin' = min(step_ex$xmin), 
+                                     'xmax' = max(step_ex$xmax), 'ymin' = min(step_ex$ymin), 'ymax' = max(step_ex$ymax)))
+
 # save table 
 write.csv(step_ex, 'data/step_extents/LA2_step_ex_w2027.csv')
 
 
-######################### create covariate dataset #################### WHERE I LEFT OFF
 
-## stack all MODIS NDVI images 
-cov <- sprc(list.files('data/modis_ssf', pattern = glob2rx('*.tif'), full.names = T))
-
-rast_list <- list()
-
-for(i in 1:length(cov)){
-  layer <- cov[i]
-  time(layer, tstep = 'days') <- as.POSIXlt(names(layer), format = '%Y_%m_%d', tz = 'Africa/Maputo')
-  print(time(layer))
-  rast_list <- c(rast_list, layer)
-}
-
-cov_col <- sprc(rast_list)
-cov_col
+###############################################################################
+###### GO TO JN TO GENERATE MODIS IMAGES USING STEP EXTENT LUT ################
+###### ONCE HAVE GENERATED IMAGES CAN RUN SCRIPT FROM TOP UNTIL FIRST BREAK ###
+###### TO GENERATE ELEPHANT DATAFRAME AS STEPS_XYT OBJECT #####################
+###############################################################################
 
 
-# https://stackoverflow.com/questions/20733555/how-to-create-a-raster-brick-with-rasters-of-different-extents
+
+######################### create covariate dataset #################### 
+
+# stack all generated MODIS images together 
+modis_images <- rast(list.files('data/modis_ssf', pattern = glob2rx('*.tif'), full.names = T))
+
+# add a time (date) attribute to the spatraster --> daily interval 
+# source: https://rdrr.io/github/rspatial/terra/man/time.html
+# source: https://stackoverflow.com/questions/73259623/how-to-index-individual-layers-from-a-spatraster-object-by-time
+time(modis_images, tstep = 'days') <- as.Date(names(modis_images)[1], format = '%Y_%m_%d', 
+                                              tz = 'Africa/Maputo') + 0:(nlyr(modis_images)-1)
+
 
 
 ########################## extract covariates ##########################
 
-# create new dataset to add covariates to and new column
-step_dataset <- eleph_mov[1:30,]
-step_dataset$ndvi_10 <- NA
-step_dataset$ndvi_50 <- NA
-step_dataset$ndvi_90 <- NA
+# create new dataset to add covariates to and new column (make sure it's steps_xyt object)
+step_dataset <- elephant_all_steps_by_path #[1:30,]
+# step_dataset$ndvi_10 <- NA
+# step_dataset$ndvi_50 <- NA
+# step_dataset$ndvi_90 <- NA
 
-# # change date string format (could be more efficient code)
-# step_dataset$start_date <- gsub('-', '_', step_dataset$start_date)    
-# step_dataset$end_date <- gsub('-', '_', step_dataset$end_date)   
-# step_dataset$start_date_prev_week <- gsub('-', '_', step_dataset$start_date_prev_week)   
-# step_dataset$end_date_prev_week <- gsub('-', '_', step_dataset$end_date_prev_week)   
+# extract covariates from correct MODIS image
 
-# extract covariates along step 
-step_dataset_along <- extract_covariates_along(step_dataset, cov[which(names(cov) == step_dataset$start_date)], name_covar = 'ndvi_along')
+# option 1: extract only end pixel of each step 
+step_dataset <- extract_covariates_var_time(step_dataset, modis_images, where = 'end', max_time = period(1, units = 'days'), name_covar = 'ndvi')
 
-step_dataset$ndvi_10[1] <- unname(quantile(step_dataset_along[[1]], probs = 0.1))
+# option 2: extract all pixels along step for correct day 
+# need to construct a loop that will retrieve correct modis image for each step
+# this is less efficient than option 1 but try it 
 
+# THIS IS WHERE I LEFT OFF 
 
-i <- rast('data/modis_ssf/2008_11_06.tif')
-i
-plot(i)
-
-
-
-# function that calculates mean value of covariates along a step 
-# Note: make sure column for covariate exists before filling it in otherwise error
-getAlongValue_10 <- function(i){
-  step_dataset$ndvi_10[i] <- quantile(step_dataset_along[[i]], probs = 0.1, names = F)
-}
-
-getAlongValue_50 <- function(i){
-  step_dataset$ndvi_50[i] <- quantile(step_dataset_along[[i]], probs = 0.5)
-}
-
-getAlongValue_90 <- function(i){
-  step_dataset$ndvi_90[i] <- mean(step_dataset_along[[i]])
-}
-
-# apply function to each step 
-step_dataset$ndvi_10 <- sapply(1:756, getAlongValue_10)
-step_dataset$ndvi_50 <- sapply(seq.int(1, nrow(step_dataset)), getAlongValue_50)
-step_dataset$ndvi_90 <- sapply(seq.int(1, nrow(step_dataset)), getAlongValue_90)
+# step_dataset$ndvi_10[1] <- unname(quantile(step_dataset_along[[1]], probs = 0.1))
+# 
+# print(step_dataset[1])
+# 
+# 
+# i <- rast('data/modis_ssf/2008_11_06.tif')
+# i
+# plot(i)
+# 
+# a <- period(1)
+# class(a)
+# 
+# 
+# # function that calculates mean value of covariates along a step 
+# # Note: make sure column for covariate exists before filling it in otherwise error
+# getAlongValue_10 <- function(i){
+#   step_dataset$ndvi_10[i] <- quantile(step_dataset_along[[i]], probs = 0.1, names = F)
+# }
+# 
+# getAlongValue_50 <- function(i){
+#   step_dataset$ndvi_50[i] <- quantile(step_dataset_along[[i]], probs = 0.5)
+# }
+# 
+# getAlongValue_90 <- function(i){
+#   step_dataset$ndvi_90[i] <- mean(step_dataset_along[[i]])
+# }
+# 
+# # apply function to each step 
+# step_dataset$ndvi_10 <- sapply(1:756, getAlongValue_10)
+# step_dataset$ndvi_50 <- sapply(seq.int(1, nrow(step_dataset)), getAlongValue_50)
+# step_dataset$ndvi_90 <- sapply(seq.int(1, nrow(step_dataset)), getAlongValue_90)
 
 
 
