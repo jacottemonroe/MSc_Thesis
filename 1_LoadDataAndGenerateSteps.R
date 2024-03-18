96bdf991c55b9140237a22dba8c7235eec646a73
jacottemonroe <jacottemonroe@gmail.com>
2024-03-18 19:24:56 -0400

    Split the main R script into cleaner R scripts that run functions for each step of the process. Only done up to MODIS image retrieval (steps 1 and 2 done, steps 4 and 5 still need to be done after figure out python script). Also created bash script to run each R script. Attempts to run the ipynb file haven't been successful so far.

---
+++ 1_LoadDataAndGenerateSteps.R
@@ -0,0 +1,81 @@
+## MSc_Thesis 
+## Jacotte Monroe 
+## 18/03/24
+
+## R script that runs functions to retrieve elephant fixes dataset for week of interest. 
+## Data is transformed into a dataset of movement steps that constitute the presence movement paths. 
+## Pseudo-absence movement paths are generated following the method specified in run settings. 
+## Script outputs: 
+##    1_a1: Dataset of all fixes from elephant of interest as track_xyt object, saved as RDS object. (intermediate output)
+##    1_a2: Dataset of fixes from week and elephant of interest as track_xyt object, saved as RDS object. (intermediate output)
+##    1_b1: Dataset of presence/pseudo-absence steps from elephant and week of interest as steps_xyt object, saved as RDS object. (intermediate output)
+
+
+###########
+## Read run settings 
+###########
+
+run_settings <- read.csv('data/single_run_settings.csv', header = F, row.names = 1)
+
+# define elephant ID
+ID <- run_settings[[1]]
+
+# define week to test 
+week <- run_settings[[2]]
+
+# define pseudo-absence path generator method --> Choices are 1) random_path_custom_distr 2) random_path_buffer_point 3) random_step
+pseudo_abs_method <- run_settings[[3]]
+
+# define run filepath 
+run_filepath <- paste0('data/', ID, '/', week, '/')
+
+# define file name of elephant fixes based on elephant ID from run settings 
+file_name <- paste0('data/elephant_etosha/elephant_fixes/preprocessed_elephant_', ID,'.csv')
+
+
+
+###########
+## Load elephant data and transform data frame into a track_xyt object
+###########
+
+# load function
+source('functions_elephant_ssf/1_a_transformingToTrackObject.R')
+
+# necessary packages to load 
+if(!('amt') %in% installed.packages()){install.packages('amt')}
+library(amt)
+
+# run function 
+transformToTrackObject(file_name, ID, week, output_directory = 'data/')
+
+print(paste('(DONE) Transforming elephant', ID, 'of week', week, 'into track object.'))
+print('Now generating steps...')
+
+
+
+###########
+## Generate the elephant presence and pseudo-absence steps dataset
+###########
+
+# load function 
+source('functions_elephant_ssf/1_b_generatingSteps.R')
+
+# load necessary libraries (commented out = necessary but already loaded from previous command)
+# if(!('amt') %in% installed.packages()){install.packages('amt')}
+# library(amt)
+if(!('dplyr') %in% installed.packages()){install.packages('dplyr')} 
+library(dplyr)
+
+if(pseudo_abs_method == 'random_path_buffer_point'){
+  if(!('sf') %in% installed.packages()){install.packages('sf')} 
+  library(sf)
+  if(!('terra') %in% installed.packages()){install.packages('terra')} 
+  library(terra)
+}
+
+# run function
+generateSteps(run_filepath, ID, week, 20, random_data_method = pseudo_abs_method, paste0('data/'))
+
+print(paste('(DONE) Generating steps for elephant', ID, 'of week', week, 'using pseudo-absence method:', pseudo_abs_method))
+print('Now creating step extent look-up table...')
+
---
+++ 2_CreateStepExtentsLUT.R
@@ -0,0 +1,49 @@
+## MSc_Thesis 
+## Jacotte Monroe 
+## 18/03/24
+
+## R script that runs a function to create a look-up table of the spatial extents in the elephant step dataset. 
+## Script takes the run settings to retrieve the step_xyt dataset and summarizes the information into a table for MODIS image retrieval.
+## The last row contains information on the first/last dates of the dataset as well as the largest spatial extent.
+## Script outputs: 
+##    2_a1: Look-up table of spatial extents and dates of the movement steps, saved as csv object. (intermediate output)
+
+
+
+
+###########
+## Read run settings 
+###########
+
+run_settings <- read.csv('data/single_run_settings.csv', header = F, row.names = 1)
+
+# define elephant ID
+ID <- run_settings[[1]]
+
+# define week to test 
+week <- run_settings[[2]]
+
+# define pseudo-absence path generator method --> Choices are 1) random_path_custom_distr 2) random_path_buffer_point 3) random_step
+pseudo_abs_method <- run_settings[[3]]
+
+# define run filepath 
+run_filepath <- paste0('data/', ID, '/', week, '/')
+
+
+
+###########
+## Create step extents look-up table for image retrieval
+###########
+
+# load function 
+source('functions_elephant_ssf/2_a_creatingStepExtentsLUT.R')
+
+# necessary packages 
+if(!('dplyr') %in% installed.packages()){install.packages('dplyr')} #for grouping in table (max/min)
+library(dplyr)
+
+# run function
+createStepExtentsLUT(run_filepath, ID, week, pseudo_abs_method, output_directory = 'data/')
+
+print(paste('(DONE) Creating step extent LUT for elephant', ID, 'of week', week))
+print('Now retrieving MODIS data...')
