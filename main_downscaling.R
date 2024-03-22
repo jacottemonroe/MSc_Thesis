200e4e5e14dede0d3e6ead06ff62b76a87ba8a26
jacottemonroe <jacottemonroe@gmail.com>
2024-03-22 13:18:38 -0400

    Modified the ipython script to successfully generate Landsat scenes without size limit issues (breaking down the scene into chunks). Then wrote code to stitch scenes back together into one landsat image.

--- main_downscaling.R
+++ main_downscaling.R
@@ -10,9 +10,9 @@ library(terra)
 if(!('ggplot2') %in% installed.packages()){install.packages('ggplot2')} # to plot timeseries
 library(ggplot2)
 
-m <- rast('data/modis/cloudmasked/2300/2014_01_26.tif')
+m <- rast('data/LA14/2260/3_b1_modis_images_downscaling_random_path_custom_distr/2013_04_18.tif')
 m
-plot(m[[1]])
+plot(m[[4]])
 
 m2 <- rast('data/modis/2300/2014_01_23.tif')
 m2
@@ -22,27 +22,170 @@ m3 <- rast('data/modis/8d/2300/2014_01_17.tif')
 m3
 plot(m3[[2]])
 
-l <- rast('data/l8/2300/LC08_179073_20140131.tif')
+l2 <- rast('data/LA26/2260/3_b2_landsat_images_downscaling_random_path_custom_distr/TL_TL/LC08_179074_20130504.tif')
+l2
+plot(is.na(l2[[4]]))
+
+
+library(ggplot2)
+library(tidyterra)
+
+
+## mosaic the landsat scenes together to create the large extent image
+# will need to loop through dates (could do this after creating the LUT?)
+
+# enter settings
+ID <- 'LA14'
+week <- 2260
+pseudo_abs_method <- 'random_path_custom_distr'
+
+# define run filepath 
+run_filepath <- paste0('data/', ID, '/', week, '/')
+
+# define landsat filepath 
+landsat_filepath <- paste0(run_filepath, '3_b2_landsat_images_downscaling_', pseudo_abs_method, '/')
+
+# list directories of scenes 
+scene_directories <- list.dirs(landsat_filepath, full.names = F)[-1]
+
+# get filenames (from LUT?)
+landsat_filenames_list <- list.files(paste0(landsat_filepath, 'BL_BL'))
+
+# loop through the different landsat image dates 
+for(landsat_filename in landsat_filenames_list){
+  # loop through the directories to load the scenes for the corresponding image date and append them to a list
+  scenes <- list()
+  for(sc_folder in scene_directories){
+    # load scene to align 
+    sc <- rast(paste0(landsat_filepath, sc_folder, '/', landsat_filename))
+    scenes <- append(scenes, list(sc))
+  }
+  
+  # stitch the scenes together to make one large landsat 8 image
+  # source: # source: https://gis.stackexchange.com/questions/224781/merge-rasters-with-different-origins-in-r
+  # source: https://rdrr.io/cran/terra/man/merge.html
+  l8_mosaic <- do.call(merge, scenes)
+  
+  # save new image 
+  writeRaster(l8_mosaic, paste0(landsat_filepath, landsat_filename), overwrite = T)
+  
+}
+
+# remove scene folders and files since now have composite image
+# source: https://stackoverflow.com/questions/28097035/how-to-remove-a-directory-in-r
+unlink(paste0(landsat_filepath, scene_directories), recursive = T)
+
+# get all dates from file names 
+# source: https://stackoverflow.com/questions/17215789/extract-a-substring-according-to-a-pattern
+l_dates <- sub('.*_17.*_', '', landsat_filenames_list)
+l_dates <- unique(sub('.tif', '', l_dates))
+
+# select images for each date and mosaic them 
+for(date in l_dates){
+  # select file names containing the date 
+  # source: https://stackoverflow.com/questions/69759984/how-can-i-subset-a-list-in-r-by-extracting-the-elements-that-contain-a-string
+  list_images <- landsat_filenames_list[grep(date, landsat_filenames_list)]
+  
+  # create empty list to store the rasters
+  scenes <- list()
+  
+  for(image in list_images){
+    sc <- rast(paste0(landsat_filepath, image))
+    scenes <- append(scenes, list(sc))
+  }
+  
+  # stitch the scenes together to make one large landsat 8 image
+  # source: # source: https://gis.stackexchange.com/questions/224781/merge-rasters-with-different-origins-in-r
+  # source: https://rdrr.io/cran/terra/man/merge.html
+  l8_mosaic <- do.call(merge, scenes)
+  
+  # save new image 
+  writeRaster(l8_mosaic, paste0(landsat_filepath, 'LC08_179073_4_', date, '.tif'), overwrite = T)
+}
+
+list_images <- landsat_filenames_list[grep('20130504', landsat_filenames_list)]
+list_images
+t <- rast(paste0(landsat_filepath, 'LC08_179073_4_20130504.tif'))
+t
+plot(t[[4]])
+
+
+
+
+tl <- rast(paste0('data/LA14/2260/3_b2_landsat_images_downscaling_random_path_custom_distr/', 'TL_TL/', landsat_filename))
+
+tr <- rast(paste0('data/LA14/2260/3_b2_landsat_images_downscaling_random_path_custom_distr/', 'top_right_image/', landsat_filename))
+br <- rast(paste0('data/LA14/2260/3_b2_landsat_images_downscaling_random_path_custom_distr/', 'bottom_right_image/', landsat_filename))
+bl <- rast(paste0('data/LA14/2260/3_b2_landsat_images_downscaling_random_path_custom_distr/', 'bottom_left_image/', landsat_filename))
+
+
+l <- rast('data/LA14/2260/3_b2_landsat_images_downscaling_random_path_custom_distr/LC08_179073_20130418.tif')
 l
 plot(is.na(l[[1]]))
+plot(l[[1]])
+
+ndvi_data <- l[[4]]
+names(ndvi_data) <- 'ndvi'
+ID <- 'LA14'
+week <- 2260
+
+ndvi_data
+
+modis_ndvi_map <- ggplot() +
+  geom_spatraster(data = ndvi_data, aes(fill = ndvi), show.legend = T) +
+  scale_fill_terrain_c(name = 'NDVI')
+
+# read step dataset
+dat <- readRDS('data/LA14/2260/1_b1_all_steps_random_path_custom_distr.RDS')
+write.csv(dat, 'data/LA14/2260/temp_all_steps.csv')
+dat <- read.csv('data/LA14/2260/temp_all_steps.csv', row.names = 1)
+dat$random_id_ <- as.factor(dat$random_id_)
+
+# add title and theme to plot
+mov_map <- modis_ndvi_map +
+  labs(title = 'Elephant Movement on NDVI', subtitle = paste(ID, week), x = "Longitude", y = "Latitude") +
+  theme_minimal()
+
+# plot presence and pseudo-absence paths on basemap by looping for each path 
+# source: https://stackoverflow.com/questions/9206110/using-png-function-not-working-when-called-within-a-function
+for(i in 1:length(unique(dat$burst_))){
+  for(j in 1:length(unique(dat$random_id_))){
+    # source: https://ggplot2.tidyverse.org/reference/aes_linetype_size_shape.html
+    mov_map <- mov_map + geom_path(data = dat[dat$case_ == F & dat$burst_ == i & dat$random_id_ == j,], aes(x = x1_, y = y1_), color = 'grey40', linewidth = 0.4, linetype = 2, show.legend = F)
+  }
+  mov_map <- mov_map + geom_path(data = dat[dat$case_ == T & dat$burst_ == i,], aes(x = x1_, y = y1_), color = 'black', linewidth = 0.5, show.legend = F)
+} 
+
+png('data/LA14/2260/random_image.png')
+mov_map
+dev.off()
+
+
 
 ############################################################################
 ############### generate Landsat and MODIS images in JN ####################
 ############## once have images i can start this script ####################
 ############################################################################
+ID <- 'LA14'
+week <- 2260
 
-week <- as.character(2300)
-path_modis <- paste0('data/modis/8d/', week, '/')
-path_landsat <- paste0('data/l8/', week, '/')
+# define run filepath 
+run_filepath <- paste0('data/', ID, '/', week, '/')
+
+path_modis <- paste0(run_filepath, '3_b1_modis_images_downscaling_random_path_custom_distr/')
+path_landsat <- paste0(run_filepath, '3_b2_landsat_images_downscaling_random_path_custom_distr/')
 
 # create LUT from Landsat and MODIS images in data folders
 # matches a Landsat image to each MODIS image (nearest Landsat image)
 source('functions_downscaling/creatingLUT.R')
 
-createLUT(path_modis, path_landsat)
+createLUT(path_modis, path_landsat, ID, week, output_directory = 'data/')
+
+
+
 
 # load LUT
-LUT <- readRDS('data/LUT.RData')
+LUT <- readRDS(paste0(run_filepath, '3_c1_MODISLandsatLUT.RData'))
 #LUT <- LUT[1:30,]
 
 # create downscaling model 
