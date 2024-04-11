459ce5c06eae51e53f7bc87f6b7b8e010518bf1e
jacottemonroe <jacottemonroe@gmail.com>
2024-04-11 01:46:27 -0400

    Wrote code that would generate a timeseries graph of the significant model coefficients

--- interpreting_results.R
+++ interpreting_results.R
@@ -5,8 +5,11 @@
 summary_results <- data.frame()
 
 # get table of runs 
-run_settings <- read.csv('data/run_settings.csv', row.names = 1)
+run_settings <- read.csv('data/run_settings_RQ2.csv', row.names = 1)
+run_settings <- run_settings[1:77,]
+#run_settings <- run_settings[run_settings$ID == 'LA14' & run_settings$downscaling == F,]
 
+suffix <- '' #'_downscaling_modis_250m'
 # loop over each run 
 for(i in 1:nrow(run_settings)){
   # retrieve the run
@@ -32,74 +35,74 @@ for(i in 1:nrow(run_settings)){
   
   ## are the VIF values of the model acceptable? 
   # retrieve the dataset
-  full_df <- read.csv(paste0(run_filepath, '6_a5_glm_full_vif_', pseudo_abs_method, '.csv'))
-  sub_df <- read.csv(paste0(run_filepath, '6_b5_glm_50p_sd_vif_', pseudo_abs_method, '.csv'))
+  #full_df <- read.csv(paste0(run_filepath, '6_a5_glm_full_vif_', pseudo_abs_method, '.csv'))
+  sub_df <- read.csv(paste0(run_filepath, '6_b5_glm_50p_sd_vif_', pseudo_abs_method, suffix, '.csv'))
   
   # check if vif values below 5
-  if(any(full_df$vif_results <5)){entry$VIF_full <- T}else{entry$VIF_full <- F}
+  #if(any(full_df$vif_results <5)){entry$VIF_full <- T}else{entry$VIF_full <- F}
   if(any(sub_df$vif_results <5)){entry$VIF_sub <- T}else{entry$VIF_sub <- F}
   
   
   ## does the glm model have a significant coefficient? 
   # retrieve dataset
-  full_df <- read.csv(paste0(run_filepath, '6_a3_glm_full_coefs_', pseudo_abs_method, '.csv'))
-  sub_df <- read.csv(paste0(run_filepath, '6_b3_glm_50p_sd_coefs_', pseudo_abs_method, '.csv'))
+  #full_df <- read.csv(paste0(run_filepath, '6_a3_glm_full_coefs_', pseudo_abs_method, '.csv'))
+  sub_df <- read.csv(paste0(run_filepath, '6_b3_glm_50p_sd_coefs_', pseudo_abs_method, suffix, '.csv'))
   
   # exclude intercept 
-  full_df <- full_df[full_df$X != '(Intercept)', ]
+  #full_df <- full_df[full_df$X != '(Intercept)', ]
   sub_df <- sub_df[sub_df$X != '(Intercept)', ]
   
   # check if pvalues below 0.05
-  if(any(full_df$Pr...z.. <=0.05)){entry$full_glm_sig_coef <- T}else{entry$full_glm_sig_coef <- F}
+  #if(any(full_df$Pr...z.. <=0.05)){entry$full_glm_sig_coef <- T}else{entry$full_glm_sig_coef <- F}
   if(any(sub_df$Pr...z.. <=0.05)){entry$sub_glm_sig_coef <- T}else{entry$sub_glm_sig_coef <- F}
   
   # select predictors that are significant with 95% confidence 
   # source: https://sparkbyexamples.com/r-programming/r-merge-vector-to-string/
-  entry$full_glm_sig_coef_which <- paste(full_df$X[full_df$Pr...z.. < 0.05], collapse = '; ')
+  #entry$full_glm_sig_coef_which <- paste(full_df$X[full_df$Pr...z.. < 0.05], collapse = '; ')
   entry$sub_glm_sig_coef_which <- paste(sub_df$X[sub_df$Pr...z.. < 0.05], collapse = '; ')
 
   ## how high is the deviance of the fitted model? is the model significant? 
   # retrieve dataset
-  full_df <- read.csv(paste0(run_filepath, '6_a4_glm_full_deviances_', pseudo_abs_method, '.csv'))
-  sub_df <- read.csv(paste0(run_filepath, '6_b4_glm_50p_sd_deviances_', pseudo_abs_method, '.csv'))
+  #full_df <- read.csv(paste0(run_filepath, '6_a4_glm_full_deviances_', pseudo_abs_method, '.csv'))
+  sub_df <- read.csv(paste0(run_filepath, '6_b4_glm_50p_sd_deviances_', pseudo_abs_method, suffix, '.csv'))
   
   # select residual deviance 
-  entry$full_glm_deviance <- full_df$residual_deviance
+  #entry$full_glm_deviance <- full_df$residual_deviance
   entry$sub_glm_deviance <- sub_df$residual_deviance
   
   # derive p-value of model from chi-square stat
   # source: https://stats.stackexchange.com/questions/340489/interpretation-of-deviance-in-logistic-model
   # source: https://www.r-bloggers.com/2022/05/calculate-the-p-value-from-chi-square-statistic-in-r/
-  full_pval <- pchisq(full_df$null_deviance - full_df$residual_deviance, full_df$null_df - full_df$residual_df, lower.tail = F)
+  #full_pval <- pchisq(full_df$null_deviance - full_df$residual_deviance, full_df$null_df - full_df$residual_df, lower.tail = F)
   sub_pval <- pchisq(sub_df$null_deviance - sub_df$residual_deviance, sub_df$null_df - sub_df$residual_df, lower.tail = F)
   
   # check if pvalues below 0.05
-  if(full_pval <=0.05){entry$full_glm_sig <- T}else{entry$full_glm_sig <- F}
+  #if(full_pval <=0.05){entry$full_glm_sig <- T}else{entry$full_glm_sig <- F}
   if(sub_pval <=0.05){entry$sub_glm_sig <- T}else{entry$sub_glm_sig <- F}
   
   
   ## does the clr model have a significant coefficient? 
   # retrieve dataset
-  full_df <- read.csv(paste0(run_filepath, '6_a1_clr_full_coefs_', pseudo_abs_method, '.csv'))
-  sub_df <- read.csv(paste0(run_filepath, '6_b1_clr_50p_sd_coefs_', pseudo_abs_method, '.csv'))
+  #full_df <- read.csv(paste0(run_filepath, '6_a1_clr_full_coefs_', pseudo_abs_method, '.csv'))
+  sub_df <- read.csv(paste0(run_filepath, '6_b1_clr_50p_sd_coefs_', pseudo_abs_method, suffix, '.csv'))
   
   # check if pvalues below 0.05
-  if(any(full_df$Pr...z.. <=0.05)){entry$full_clr_sig_coef <- T}else{entry$full_clr_sig_coef <- F}
+  #if(any(full_df$Pr...z.. <=0.05)){entry$full_clr_sig_coef <- T}else{entry$full_clr_sig_coef <- F}
   if(any(sub_df$Pr...z.. <=0.05)){entry$sub_clr_sig_coef <- T}else{entry$sub_clr_sig_coef <- F}
   
   # select predictors that are significant with 95% confidence 
   # source: https://sparkbyexamples.com/r-programming/r-merge-vector-to-string/
-  entry$full_clr_sig_coef_which <- paste(full_df$X[full_df$Pr...z.. < 0.05], collapse = '; ')
+  #entry$full_clr_sig_coef_which <- paste(full_df$X[full_df$Pr...z.. < 0.05], collapse = '; ')
   entry$sub_clr_sig_coef_which <- paste(sub_df$X[sub_df$Pr...z.. < 0.05], collapse = '; ')
 
   ## what is the concordance? how much does it vary by? 
   # retrieve dataset
-  full_df <- read.csv(paste0(run_filepath, '6_a2_clr_full_tests_', pseudo_abs_method, '.csv'))
-  sub_df <- read.csv(paste0(run_filepath, '6_b2_clr_50p_sd_tests_', pseudo_abs_method, '.csv'))
+  #full_df <- read.csv(paste0(run_filepath, '6_a2_clr_full_tests_', pseudo_abs_method, '.csv'))
+  sub_df <- read.csv(paste0(run_filepath, '6_b2_clr_50p_sd_tests_', pseudo_abs_method, suffix, '.csv'))
   
   # select concordance and SE
-  entry$full_clr_concord <- full_df$concordance[1]
-  entry$full_clr_concord_se <- full_df$concordance[4]
+  # entry$full_clr_concord <- full_df$concordance[1]
+  # entry$full_clr_concord_se <- full_df$concordance[4]
   entry$sub_clr_concord <- sub_df$concordance[1]
   entry$sub_clr_concord_se <- sub_df$concordance[4]
   
@@ -108,7 +111,68 @@ for(i in 1:nrow(run_settings)){
 }
 
 # results for running 13 elephants on week 2075 with three methods 
-write.csv(summary_results, 'output/summary_results_13E_w2075_3M.csv')
+write.csv(summary_results, 'output/summary_results_RQ2.csv')
+
+
+##### FOR RQ2 TIMESERIES
+
+# retrieves summary results for only sig models GLM 
+srsig <- summary_results[summary_results$sub_glm_sig == T,] # these are the models i want to retrieve coefs for 
+
+# get weeks of sig models 
+sigw <- srsig$week
+
+# get run table for only weeks that are sig 
+rsig <- run_settings[run_settings$week %in% sigw,]
+
+tsdf <- data.frame()
+
+for(i in 1:nrow(rsig)){
+  
+  # get run settings
+  ID <- rsig$ID[i]
+  week <- rsig$week[i]
+  method <- rsig$pseudo_abs_method[i]
+  
+  # define filepaths
+  data_path <- paste0('data/', ID, '/', week, '/')
+  output_path <- paste0('output/', ID, '/', week, '/')
+  
+  # retrieve date of week 
+  dfile <- read.csv(paste0(data_path, '2_a1_step_extents_LUT_', method, '.csv'), row.names = 1)
+  date <- dfile$start_date[1]
+  
+  # retrieve coefs glm 
+  c <- read.csv(paste0(output_path, '6_b3_glm_50p_sd_coefs_random_path_custom_distr', suffix, '.csv'))
+  c <- c[2:nrow(c),]
+  
+  # specify if sig or not with 90% confidence (so pvalue < 0.1)
+  c$Pr...z..[as.numeric(c$Pr...z..) < 0.1] <- 'sig'
+  c$Pr...z..[as.numeric(c$Pr...z..) >= 0.1] <- 'not sig'
+  
+  # create new entry with coefs and pvalues 
+  # entry <- data.frame(ID = ID, week = week, method = method, date = date, ndvi_50 = c$Estimate[1], 
+  #                     ndvi_sd = c$Estimate[2], ndvi_rate_50 = c$Estimate[3], ndvi_rate_sd = c$Estimate[4], 
+  #                     pval.ndvi_50 = c$Pr...z..[1], pval.ndvi_sd = c$Pr...z..[2], pval.ndvi_rate_50 = c$Pr...z..[3], pval.ndvi_rate_sd = c$Pr...z..[4])
+  entry <- data.frame(ID = ID, week = week, method = method, date = date, predictor = c[,1], value = c$Estimate, significance = c$Pr...z..)
+  
+  tsdf <- rbind(tsdf, entry)
+}
+
+
+#library(ggplot2)
+ggplot(data = tsdf, aes(x = as.Date(date, tz = 'Africa/Maputo'), y = value, group = predictor, color = predictor)) + 
+  geom_line() + geom_point(data = tsdf[tsdf$significance == 'sig',], aes(y = value), shape = 8) + 
+  geom_hline(yintercept = 0) + 
+  # source: https://ggplot2.tidyverse.org/reference/facet_grid.html
+  facet_grid(vars(predictor), scale = 'free') + 
+  xlab('Time') + ylab('Coefficient Value') + ggtitle('Timeseries of predictors for LA14 from August 2009 to June 2010')
+  
+
+
+
+
+######################################### end RQ2 code 
 
 
 
@@ -650,7 +714,7 @@ for(i in 1:nrow(r)){
   step5_files <- c('5_a1_elephant_movement_map_random_path_custom_distr.png')
   step6_files <- c('6_b0_clr_50p_sd_model_random_path_custom_distr.RDS', '6_b0_glm_50p_sd_model_random_path_custom_distr.RDS', 
                    '6_b1_clr_50p_sd_coefs_random_path_custom_distr.csv', '6_b2_clr_50p_sd_tests_random_path_custom_distr.csv', 
-                   '6_a1_correlation_matrix_random_path_custom_distr.csv', '6_b3_glm_50p_sd_coefs_random_path_custom_distr.csv', 
+                   '6_a1_correlation_matrix_random_path_custom_distr.png', '6_b3_glm_50p_sd_coefs_random_path_custom_distr.csv', 
                    '6_b4_glm_50p_sd_deviances_random_path_custom_distr.csv', '6_b5_glm_50p_sd_vif_random_path_custom_distr.csv')
   step7_files <- c('7_b1_50p_sd_plot_log_odds_random_path_custom_distr.png', '7_b2_50p_sd_plot_odd_ratios_random_path_custom_distr.png', 
                    '7_b3_50p_sd_plot_curve_random_path_custom_distr.png')
@@ -660,9 +724,9 @@ for(i in 1:nrow(r)){
   if(all(step2_files %in% data_files)){step2 = T}else{step2 = F}
   if(all(step3_files %in% data_files) & length(list.files(paste0(data_path, step3_files))) == 15){step3 = T}else{step3 = F}
   if(all(step4_files %in% data_files)){step4 = T}else{step4 = F}
-  if(all(step5_files %in% data_files)){step5 = T}else{step5 = F}
-  if(all(step6_files %in% data_files)){step6 = T}else{step6 = F}
-  if(all(step7_files %in% data_files)){step7 = T}else{step7 = F}
+  if(all(step5_files %in% output_files)){step5 = T}else{step5 = F}
+  if(all(step6_files %in% output_files)){step6 = T}else{step6 = F}
+  if(all(step7_files %in% output_files)){step7 = T}else{step7 = F}
   
   # check if all steps complete 
   if(all(c(step1, step2, step3, step4, step5, step6, step7) == T)){complete = T}else{complete = F}
@@ -673,3 +737,23 @@ for(i in 1:nrow(r)){
   df <- rbind(df, entry)
   
 }
+
+dfr <- df$week[df$step3 == F]
+print(dfr)
+
+rr <- r[r$week %in% dfr,]
+
+write.csv(rr, 'data/run_settings_RQ2_rerun.csv')
+
+# 
+# s <- readRDS('data/LA14/2112/1_b1_all_steps_random_path_custom_distr.RDS')
+# ss <- readRDS('data/LA14/2112/1_a2_elephant_track_xyt.RDS')
+# 
+# 
+# 
+# r <- r[2:nrow(r),]
+# rownames(r) <- 1:nrow(r)
+# write.csv(r, 'data/run_settings_RQ2.csv')
+# r <- read.csv('data/run_settings_RQ2.csv', row.names = 1)
+# r$X <- NULL
+
